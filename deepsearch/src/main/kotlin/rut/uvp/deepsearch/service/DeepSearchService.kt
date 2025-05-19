0ab0aaf10ead537a73a9a5130e069013b0e3ad76
Poroshin Gleb <gleb.poroshin@lemanapro.ru>
2025-05-19 23:48:16 +0300

    Add service

--- deepsearch/src/main/kotlin/rut/uvp/deepsearch/service/DeepSearchService.kt
+++ deepsearch/src/main/kotlin/rut/uvp/deepsearch/service/DeepSearchService.kt
@@ -1,5 +1,26 @@
 package rut.uvp.deepsearch.service
 
-class DeepSearchService {
-    // TODO: Основная бизнес-логика поиска
-} 
+import org.slf4j.LoggerFactory
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.stereotype.Service
+import rut.uvp.deepsearch.client.DuckDuckGoClient
+import rut.uvp.deepsearch.extractor.WebContentExtractor
+
+@Service
+class DeepSearchService(
+    private val duckDuckGoClient: DuckDuckGoClient,
+    private val webContentExtractor: WebContentExtractor
+) {
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    @Cacheable("deepSearchResults")
+    fun deepSearch(query: String): String? {
+        log.info("DeepSearch started for query: '{}'", query)
+        val url = duckDuckGoClient.searchFirstLink(query)
+        if (url.isNullOrBlank()) {
+            log.warn("No result URL found for query: '$query'")
+            return null
+        }
+        return webContentExtractor.extractPlainText(url)
+    }
+}
