3baa7cb299394bd48f58fb25b4578d8928866af3
Poroshin Gleb <gleb.poroshin@lemanapro.ru>
2025-05-19 23:46:52 +0300

    Add DuckDuckGoClient

--- deepsearch/src/main/kotlin/rut/uvp/deepsearch/client/DuckDuckGoClient.kt
+++ deepsearch/src/main/kotlin/rut/uvp/deepsearch/client/DuckDuckGoClient.kt
@@ -1,5 +1,32 @@
 package rut.uvp.deepsearch.client
 
-class DuckDuckGoClient {
-    // TODO: Взаимодействие с DuckDuckGo
-} 
+import org.jsoup.Jsoup
+import org.slf4j.LoggerFactory
+import org.springframework.retry.annotation.Backoff
+import org.springframework.retry.annotation.Retryable
+import org.springframework.stereotype.Component
+import org.springframework.web.client.RestTemplate
+import rut.uvp.deepsearch.config.DeepSearchProperties
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
+@Component
+class DuckDuckGoClient(
+    private val restTemplate: RestTemplate,
+    private val props: DeepSearchProperties
+) {
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    @Retryable(maxAttempts = 3, backoff = Backoff(delay = 1000L, multiplier = 2.0))
+    fun searchFirstLink(query: String): String? {
+        val encodedQuery = URLEncoder.encode(query, StandardCharsets.UTF_8)
+        val url = "${props.searchUrl}?q=$encodedQuery"
+        log.debug("DuckDuckGo search URL: {}", url)
+        val response = restTemplate.getForEntity(url, String::class.java)
+        val document = Jsoup.parse(response.body ?: return null)
+        val linkElement = document.selectFirst("a.result__a")
+        val link = linkElement?.attr("href")
+        log.info("First search result for '$query': $link")
+        return link
+    }
+}
